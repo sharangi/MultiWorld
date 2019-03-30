cdf58f404ee8f1678b7ae5ea5539696913745f8f
Som <3250805+sharangi@users.noreply.github.com>
2019-03-30 12:26:54 -0700

    Add Transformer model and TransformersController

---
+++ MultiWorld/Controllers/TransformersController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiWorld.Models;
+
+namespace MultiWorld.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransformersController : ControllerBase
+    {
+        // GET: api/Transformers
+        [HttpGet]
+        public IEnumerable<Transformer> Get()
+        {
+            return null;
+        }
+
+        // GET: api/Transformers/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(Guid id)
+        {
+            return "value";
+        }
+
+        // POST: api/Transformers
+        [HttpPost]
+        public void Post([FromBody] Transformer transformer)
+        {
+        }
+
+        // PUT: api/Transformers/5
+        [HttpPut("{id}")]
+        public void Put(Guid id, [FromBody] Transformer transformer)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(Guid id)
+        {
+        }
+    }
+}
---
+++ MultiWorld/Models/Transformer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MultiWorld.Models
+{
+
+    public enum AllegianceType
+    {
+        Autobot = 1,
+        Decepticon = 2
+    }
+    public class Transformer
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime InsertTime { get; set; } = DateTime.UtcNow;
+
+        public DateTime? LastUpdateTime { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public AllegianceType Allegiance { get; set; }
+
+        [Range(1, 10)]
+        public int Strength { get; set; }
+
+        [Range(1, 10)]
+        public int Intelligence { get; set; }
+
+        [Range(1, 10)]
+        public int Speed { get; set; }
+
+        [Range(1, 10)]
+        public int Endurance { get; set; }
+
+        [Range(1, 10)]
+        public int Rank { get; set; }
+
+        [Range(1, 10)]
+        public int Courage { get; set; }
+
+        [Range(1, 10)]
+        public int Firepower { get; set; }
+
+        [Range(1, 10)]
+        public int Skill { get; set; }
+
+        public int GetScore()
+        {
+            return this.Strength + this.Intelligence + this.Speed + this.Endurance + this.Rank + this.Courage + this.Firepower + this.Skill;
+        }
+
+    }
+}
